b2dded6e3e1ff5af5816f450a874333f0769d58b
hetbhatt69 <het28bhatt@gmail.com>
2026-02-20 00:54:36 +0530

    fixed database connection

--- db.php
+++ db.php
@@ -1,19 +1,22 @@
 <?php
 
-$host = "db.YOUR_SUPABASE_ID.supabase.co";
+$host = "db.YOUR_PROJECT_ID.supabase.co";
+$port = "5432";
 $dbname = "postgres";
 $user = "postgres";
 $password = "YOUR_DB_PASSWORD";
 
 try {
     $conn = new PDO(
-        "pgsql:host=$host;port=5432;dbname=$dbname",
+        "pgsql:host=$host;port=$port;dbname=$dbname",
         $user,
         $password
     );
+
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
+
     echo json_encode([
         "status"=>"error",
         "message"=>"DB connection failed"
